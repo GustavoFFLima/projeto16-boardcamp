997c9ea9e507883249163fe7957fad2533827d80
GUSTAVO FERNANDO DE FRAZÃO LIMA <frazao39@gmail.com>
2023-02-10 01:17:49 -0300

    Fixed: status

--- src/controller/gamesController.js
+++ src/controller/gamesController.js
@@ -3,9 +3,9 @@ import { db } from "../config/database.js"
 export const getGames = async (req, res) => {
     try {
         const games = await db.query("SELECT * FROM games");
-        res.status(200).send(games.rows)
+        res.status(201).send(games.rows)
     } catch (error) {
-        res.status(500).send(error)
+        res.status(500).send(error.message)
     }
 };
 
@@ -16,6 +16,6 @@ export const postGames = async (req, res) => {
        await db.query(` INSERT INTO games (name,image,"stockTotal","pricePerDay") VALUE ($!, $2, $3, $4)`, [ name, image, stockTotal, pricePerDay ]);
         res.sendstatus(200)
     } catch (error) {
-        res.status(500).send(error)
+        res.status(500).send(error.message)
     }
 };
