0ef305fd1c436ed5303ae21bf577e5bd520fedd4
GUSTAVO FERNANDO DE FRAZÃO LIMA <frazao39@gmail.com>
2023-02-10 16:23:03 -0300

    fix: post

--- src/controller/gamesController.js
+++ src/controller/gamesController.js
@@ -4,18 +4,19 @@ export const getGames = async (req, res) => {
     try {
         const games = await db.query("SELECT * FROM games");
         res.status(201).send(games.rows)
-        console.log(games)
     } catch (error) {
         res.status(500).send(error.message)
     }
 };
 
 export const postGames = async (req, res) => {
-    const { name, image, stockTotal, pricePerDay } = res.body;
+    const { name, image, stockTotal, pricePerDay } = req.body;
 
+    console.log(games)
     try {
-       await db.query(` INSERT INTO games (name,image,"stockTotal","pricePerDay") VALUE ($!, $2, $3, $4)`, [ name, image, stockTotal, pricePerDay ]);
-        res.sendstatus(200)
+       const game = await db.query(` INSERT INTO games (name,image,"stockTotal","pricePerDay") VALUES ($1, $2, $3, $4)`, [ name, image, stockTotal, pricePerDay ]);
+       console.log(game)
+        res.sendStatus(200)
     } catch (error) {
         res.status(500).send(error.message)
     }
