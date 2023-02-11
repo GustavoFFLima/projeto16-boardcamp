07af0952c91f2b0fa23c98c20c26708cd435d45f
GUSTAVO FERNANDO DE FRAZÃO LIMA <frazao39@gmail.com>
2023-02-11 10:36:31 -0300

    Feat: validation games

--- src/controller/gamesController.js
+++ src/controller/gamesController.js
@@ -1,4 +1,5 @@
 import { db } from "../config/database.js"
+import { gamesSchema } from "../model/GamesSchema.js"
 
 export const getGames = async (req, res) => {
     try {
@@ -11,6 +12,16 @@ export const getGames = async (req, res) => {
 
 export const postGames = async (req, res) => {
     const { name, image, stockTotal, pricePerDay } = req.body;
+
+    const validation = gamesSchema.validate({ pick:  [ name, image, stockTotal, pricePerDay ], abortEarly: false})
+    
+    if (validation.error) {
+        const erros = validation.error.details.map((err) => {
+            return err.message
+        })
+        return res.status(422).send(erros)
+    }
+
     try {
       await db.query(` INSERT INTO games (name,image,"stockTotal","pricePerDay") VALUES ($1, $2, $3, $4)`, [ name, image, stockTotal, pricePerDay ]);
         res.sendStatus(201)
