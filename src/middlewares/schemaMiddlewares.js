921a906568410c1e2a911a16ba562e86b7fff408
GUSTAVO FERNANDO DE FRAZÃO LIMA <frazao39@gmail.com>
2023-02-13 10:35:14 -0300

    Fixes

--- src/middlewares/schemaMiddlewares.js
+++ src/middlewares/schemaMiddlewares.js
@@ -3,7 +3,7 @@ export function validitySchema(schema) {
         const { error } = schema.validate(req.body, { abortEarly: false });
         if(error) {
             return res.status(400).send({
-                messae: error.details.map((d) => d.message)
+                message: error.details.map((err) => err.message)
             })
             next();
         }
