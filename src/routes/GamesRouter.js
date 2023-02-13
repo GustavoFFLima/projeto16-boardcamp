f5a5302d4024b6f6c429dc47419df85c7c39c8f4
GUSTAVO FERNANDO DE FRAZÃO LIMA <frazao39@gmail.com>
2023-02-13 10:27:00 -0300

    asd

--- src/routes/GamesRouter.js
+++ src/routes/GamesRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import { validitySchema } from "../middlewares/schemaMiddlewares.js"
+import { gamesSchema } from "../model/GamesSchema.js"
 import { getGames, postGames} from "../controller/gamesController.js"
 
 const gamesRouter = Router()
 
 gamesRouter.get("/games", getGames)
-gamesRouter.post("/games", validitySchema, postGames)
+gamesRouter.post("/games", validitySchema(gamesSchema), postGames)
 
 export default gamesRouter
